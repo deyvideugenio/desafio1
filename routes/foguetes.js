94be2bb147bd4ae8cc7046a82fa9e4686cd07209
Deyvid Eugenio <deyvid.ae@gmail.com>
2024-04-04 13:52:39 -0300

    corrigindo erros

--- routes/foguetes.js
+++ routes/foguetes.js
@@ -1,24 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET foguetes listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource GET');
 });
-
 /* PUT foguetes listing. */
 router.put('/', function(req, res, next) {
-    res.send('respond with a resource PUT');
-  });
-
-  /* POST foguetes listing. */
+  res.send('respond with a resource PUT');
+});
+/* POST foguetes listing. */
 router.post('/', function(req, res, next) {
-    res.send('respond with a resource POST');
-  });
-
-  /* DELETE foguetes listing. */
+  res.send('respond with a resource POST');
+});
+/* DELETE foguetes listing. */
 router.delete('/', function(req, res, next) {
-    res.send('respond with a resource DELETE');
-  });
-      
+  res.send('respond with a resource DELETE');
+});
+
 module.exports = router;
