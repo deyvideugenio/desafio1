f02764c3af3c35981609556746847ab36a3b616d
Deyvid Eugenio <deyvid.ae@gmail.com>
2024-04-05 23:32:45 -0300

    criando configurações

---
+++ routes/produtos.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const produtoControllers = require('../controllers/produtoControllers');
+
+/* GET produtos listing. */
+router.get('/', produtoControllers.findAll);
+/* PUT produtos listing. */
+router.put('/', protudoControllers.update);
+/* POST produtos listing. */
+router.post('/', produtoControllers.save);
+/* DELETE produtos listing. */
+router.delete('/:id', produtoControllers.remove);
+
+module.exports = router;
---
+++ services/produtoService.js
@@ -0,0 +1,33 @@
+const connection = require('../configs/dbConfiguration');
+const findAll = async () => {
+const produtos = await (await connection)
+ .execute('SELECT * FROM produtos');
+return produtos[0];
+}
+
+const update = async (produtos) => {
+    const query = 'UPDATE produtos SET nome = ?, descrição = ?, preço = ?, data_atualizado = ? WHERE id = ?';
+    const isOk = await (await connection).execute(query,
+[produtos.nome, produtos.descricao, produtos.preco, produtos.data_atualizado, cliente.id]);
+    return isOk[0].affectedRows === 1;
+}
+
+const save = async (produtos) => {
+    const query = 'INSERT INTO produtos(nome, descrição, preço, data_atualizdo) VALUES (?, ?, ?, ?)';
+    const isOk = await (await connection).execute(query,
+[produtos.nome, produtos.descricao, produtos.preco, produtos.data_atualizado]);
+    return isOk[0].affectedRows === 1;
+}
+
+const remove = async (id) => {
+    const query = 'DELETE FROM produtos WHERE id = ?';
+    const isOk = await (await connection).execute(query, [id]);
+    return isOk[0].affectedRows === 1;
+}
+
+module.exports = {
+    findAll,
+    save,
+    remove,
+    update
+};
